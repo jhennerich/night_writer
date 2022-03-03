b4d59f6e552ab471cd1ff53645d4a4cacb1bd6f8
John Hennerich <john.hennerich@gmail.com>
2022-03-03 16:12:37 -0700

    Feat: add tests to iter1 spec

--- spec/iteration_1_spec.rb
+++ spec/iteration_1_spec.rb
@@ -3,4 +3,15 @@ require 'rspec'
 require 'spec_helper'
 
 RSpec.describe NightWriter do
+  it 'exists' do
+  end
+
+  it 'outputs to $stdout when called with no arguments' do
+  end
+
+  it 'outputs to $stdout when called with one argument' do
+  end
+
+  it 'outputs to $stdout when called with two argument' do
+  end
 end
