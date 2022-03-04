775081b01e68e866bb3710aea0dae6a288f864ea
John Hennerich <john.hennerich@gmail.com>
2022-03-03 17:00:10 -0700

    Feat: NightWriter and FileInteraction spec tests

---
+++ lib/night_writer.rb
@@ -0,0 +1,10 @@
+require 'pry'
+class NightWriter
+
+  def initialize()
+  end
+
+  def file
+#  handle = File.open(ARGV[0], "r")
+  end
+end
--- spec/iteration_1_spec.rb
+++ spec/iteration_1_spec.rb
@@ -1,9 +1,12 @@
 require 'pry'
 require 'rspec'
 require 'spec_helper'
+require './lib/night_writer'
 
 RSpec.describe NightWriter do
   it 'exists' do
+    night_writer = NightWriter.new()
+    expect(night_writer).to be_a (NightWriter)
   end
 
   it 'outputs to $stdout when called with no arguments' do
@@ -13,5 +16,12 @@ RSpec.describe NightWriter do
   end
 
   it 'outputs to $stdout when called with two argument' do
+  end
+end
+
+RSpec.describe FileInteraction do
+  it 'exists' do
+    output_to_file = FileInteraction.new()
+    expect(output_to_file).to be_a (FileInteraction)
   end
 end
