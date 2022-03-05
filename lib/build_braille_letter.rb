93f4e53db20e74abbb96ed88880e83ef94b3cb9e
John Hennerich <john.hennerich@gmail.com>
2022-03-05 12:08:34 -0700

    Feat: build_braille_letter working but verical not horizontal

--- lib/build_braille_letter.rb
+++ lib/build_braille_letter.rb
@@ -11,10 +11,12 @@ class BuildBrailleLetter
     @bottom_row = []
     @braille_value_array =[]
     @braille_lines =[]
+    @counter = 0
   end
 
   def change_to_braille
 
+    # single letter to change
     if @letters_to_convert.class == String
       @braille_value_array = get_braille_value[@letters_to_convert]
       @top_row = @braille_value_array.shift
@@ -23,15 +25,28 @@ class BuildBrailleLetter
       @braille_lines = @top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n"
 
     else
-      @letters_to_convert.each do |letter|
-        @braille_value_array = get_braille_value[letter]
-        @top_row = @braille_value_array.shift
-        @mid_row = @braille_value_array.shift
-        @bottom_row = @braille_value_array.shift
-        @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row)
-        @letters_to_convert.shift
+      @counter = @letters_to_convert.count
+
+      (1..(@counter)).each do |insert_newline|
+        @letters_to_convert.each do |letter|
+          @braille_value_array = get_braille_value[letter]
+          @top_row = @braille_value_array.shift
+          @mid_row = @braille_value_array.shift
+          @bottom_row = @braille_value_array.shift
+#puts "insert_newline value #{insert_newline % 40}"
+          if insert_newline % 40 == 0
+            @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n")
+            @letters_to_convert.shift
+          else
+            @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row)
+            @letters_to_convert.shift
+          end
+        end
       end
     end
-    @braille_lines
+    #make sure last element has \n"
+    @braille_lines[@counter -1] = (@top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n")
+    return @braille_lines
+#    return @braille_lines.reduce(:+)
   end
 end
--- spec/file_interaction_spec.rb
+++ spec/file_interaction_spec.rb
@@ -5,29 +5,39 @@ require './lib/build_braille_letter'
 
 RSpec.describe FileInteraction do
 
-  it 'exists' do
+  xit 'exists' do
     file_interaction = FileInteraction.new
     expect(file_interaction).to be_a (FileInteraction)
   end
 
-  it 'can read the data file' do
+  xit 'can read the data file' do
     message_data = File.read("./message_test.txt").chomp
     expect(message_data).to eq("a")
   end
 
-  it 'can write to the braille file' do
+  xit 'can write to the braille file' do
     message_data = File.read("./message_test.txt").chomp
     File.write("braille.txt", message_data, mode: "w")
     braille_file = File.read("braille.txt").chomp
     expect(braille_file).to eq("a")
   end
 
-  it '#write_to_output_file can write a single letterto braille to file' do
+  xit '#write_to_output_file can write a single letterto braille to file' do
     fi = FileInteraction.new
     bbl = BuildBrailleLetter.new("a")
     line_to_write = bbl.change_to_braille
 
     fi.write_to_output_file(line_to_write)
     expect(File.read("./braille.txt")).to eq(line_to_write)
+  end
+
+  it '#write_to_output_file can write many letters to braille to file' do
+    fi = FileInteraction.new
+    bbl = BuildBrailleLetter.new(["a","b"])
+    line_to_write = bbl.change_to_braille.join
+#  binding.pry
+
+    fi.write_to_output_file(line_to_write)
+#    expect(File.read("./braille.txt")).to eq(line_to_write)
   end
 end
