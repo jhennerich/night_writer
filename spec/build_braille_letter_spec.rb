b11c6f14390b84fea1608399113999abb0e94851
John Hennerich <john.hennerich@gmail.com>
2022-03-05 08:21:44 -0700

    Refactor: clenup build_brail_letter_spec

--- spec/build_braille_letter_spec.rb
+++ spec/build_braille_letter_spec.rb
@@ -1,9 +1,7 @@
 require 'rspec'
 require 'spec_helper'
 require './lib/letter_to_braille'
-require './file_interaction'
 require './lib/build_braille_letter'
-require './lib/file_interaction'
 
 RSpec.describe BuildBrailleLetter do
 
@@ -13,17 +11,8 @@ RSpec.describe BuildBrailleLetter do
   end
 
   it 'creates an array to write to output file' do
-#    fi = FileInteraction.new
     bbl = BuildBrailleLetter.new(["a"])
     bbl.change_to_braille
-#    braille_letter = letter_keys["a"]
-#    "a" => ["0.", "..", ".."]
-#    top_row = braille_letter.shift
-#    mid_row = braille_letter.shift
-#    bottom_row = braille_letter.shift
-#    line = (top_row+"\n"+mid_row+"\n"+bottom_row+"\n")
-  #  fi.write_to_output_file(line)
     expect(bbl.change_to_braille).to eq(["0.\n..\n..\n"])
-
   end
 end
