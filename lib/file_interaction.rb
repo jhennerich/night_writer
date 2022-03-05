ac32ad4db7aa05be662736e291bdd103ad3d5919
John Hennerich <john.hennerich@gmail.com>
2022-03-04 20:50:20 -0700

    Feat: devlop file_interaction to write braille letter

--- lib/file_interaction.rb
+++ lib/file_interaction.rb
@@ -1,10 +1,22 @@
 class FileInteraction
   include LetterToBraille
-  attr_reader :file_name, :byte_size
+  attr_reader :file_name, :byte_size, :message_data
 
   def initialize()
     @file_name = file_name
-    @byte_size = byte_size
+    @byte_size = self.byte_size
+    @message_data = self.read_input_file
   end
 
+  def read_input_file
+    File.read(ARGV[0]).chomp
+  end
+
+  def read_byte_size
+    File.size(ARGV[0])
+  end
+
+  def write_to_output_file
+    File.write("braille.txt", @message_data, mode: "w")
+  end
 end
