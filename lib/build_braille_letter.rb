c7ccc6c795b0c7be90e22c7a6c9c6c2239c57d7a
John Hennerich <john.hennerich@gmail.com>
2022-03-04 23:59:29 -0700

    Feat: build_braille_letter development

--- lib/build_braille_letter.rb
+++ lib/build_braille_letter.rb
@@ -2,4 +2,27 @@ require './lib/letter_to_braille'
 
 class BuildBrailleLetter
   include LetterToBraille
+
+  def initialize(letters_to_convert)
+    @letters_to_convert = letters_to_convert
+    @top_row = []
+    @mid_row = []
+    @bottom_row = []
+    @braille_letter =[]
+    @braille_lines =[]
+  end
+
+  def change_to_braille
+    @letters_to_convert.each do |letter|
+      @braille_letter << letter_keys[letter]
+      @braille_letter.each do |element|
+        @top_row = element.shift
+        @mid_row = element.shift
+        @bottom_row = element.shift
+      end
+      @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n")
+    binding.pry
+    end
+    return @braille_lines
+  end
 end
--- spec/build_braille_letter_spec.rb
+++ spec/build_braille_letter_spec.rb
@@ -7,14 +7,23 @@ require './lib/file_interaction'
 
 RSpec.describe BuildBrailleLetter do
 
+  it 'exsits' do
+    bbl = BuildBrailleLetter.new(["a"])
+    expect(bbl).to be_a(BuildBrailleLetter)
+  end
+
   it 'creates an array to write to output file' do
-    fi = FileInteraction.new
-    bbl = BuildBrailleLetter.new
-    braille_letter = bbl.letter_keys["a"]
-    top_row = braille_letter.shift
-    mid_row = braille_letter.shift
-    bottom_row = braille_letter.shift
-    line = (top_row+"\n"+mid_row+"\n"+bottom_row+"\n")
-    fi.write_to_output_file(line)
+#    fi = FileInteraction.new
+    bbl = BuildBrailleLetter.new(["a"])
+    bbl.change_to_braille
+#    braille_letter = letter_keys["a"]
+#    "a" => ["0.", "..", ".."]
+#    top_row = braille_letter.shift
+#    mid_row = braille_letter.shift
+#    bottom_row = braille_letter.shift
+#    line = (top_row+"\n"+mid_row+"\n"+bottom_row+"\n")
+  #  fi.write_to_output_file(line)
+    expect(bbl.change_to_braille).to eq(["0.\n..\n..\n"])
+
   end
 end
