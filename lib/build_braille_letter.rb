ad219ae3c588cdd81aa1396f98338600b6bdd0b5
John Hennerich <john.hennerich@gmail.com>
2022-03-05 12:54:05 -0700

    Feat: file_interaction writing muliple letters

--- lib/build_braille_letter.rb
+++ lib/build_braille_letter.rb
@@ -26,27 +26,27 @@ class BuildBrailleLetter
 
     else
       @counter = @letters_to_convert.count
-
-      (1..(@counter)).each do |insert_newline|
-        @letters_to_convert.each do |letter|
-          @braille_value_array = get_braille_value[letter]
-          @top_row = @braille_value_array.shift
-          @mid_row = @braille_value_array.shift
-          @bottom_row = @braille_value_array.shift
-#puts "insert_newline value #{insert_newline % 40}"
-          if insert_newline % 40 == 0
-            @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n")
-            @letters_to_convert.shift
-          else
-            @braille_lines << (@top_row+"\n"+@mid_row+"\n"+@bottom_row)
-            @letters_to_convert.shift
-          end
-        end
+      @letters_to_convert.each do |letter|
+        @braille_value_array = get_braille_value[letter]
+        @top_row << @braille_value_array.shift
+        @mid_row << @braille_value_array.shift
+        @bottom_row << @braille_value_array.shift
       end
+      #(1..(@counter)).each do |insert_newline|
+#puts "insert_newline value #{insert_newline % 40}"
+#        if insert_newline % 40 == 0
+#          @braille_lines << (@top_row.join)+"\n"+(@mid_row.join)+"\n"+(@bottom_row.join)+"\n"
+#          @letters_to_convert.shift
+#        else
+#          @braille_lines << (@top_row.join)+"\n"+(@mid_row.join)+"\n"+(@bottom_row.join)
+#          @letters_to_convert.shift
+        #end
+      #end
     end
     #make sure last element has \n"
-    @braille_lines[@counter -1] = (@top_row+"\n"+@mid_row+"\n"+@bottom_row+"\n")
-    return @braille_lines
-#    return @braille_lines.reduce(:+)
+#    @braille_lines[@counter -1] = ((@top_row.join)+"\n"+(@mid_row.join)+"\n"+(@bottom_row.join)+"\n")
+    @braille_lines << (@top_row.join)+"\n"+(@mid_row.join)+"\n"+(@bottom_row.join)+"\n"
+    binding.pry
+    @braille_lines
   end
 end
