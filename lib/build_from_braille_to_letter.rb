6250d98aea20f011b7a25ae51ca8bfca3df7f1b1
John Hennerich <john.hennerich@gmail.com>
2022-03-05 22:46:34 -0700

    Feat: develop build_from_braille

--- lib/build_from_braille_to_letter.rb
+++ lib/build_from_braille_to_letter.rb
@@ -1,10 +1,19 @@
+require 'pry'
+
 class BuildFromBraille
 
-  def initialize(argument)
-    @argument = argument
+  def initialize(braille_data)
+    @braille_data = braille_data
+    @braille_lines = []
+    @counter = 0
+
   end
 
   def braille_to_letter
+    @braille_lines = @braille_data.split("\n")
+    @counter = @braille_lines[0].length/2
+    binding.pry
+
   end
 
 end
