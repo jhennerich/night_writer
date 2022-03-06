821604fec20a79471f06b4882030da206fd4a521
John Hennerich <john.hennerich@gmail.com>
2022-03-05 23:08:11 -0700

    Feat: develop braille_to_letter method

--- lib/build_from_braille_to_letter.rb
+++ lib/build_from_braille_to_letter.rb
@@ -6,12 +6,18 @@ class BuildFromBraille
     @braille_data = braille_data
     @braille_lines = []
     @counter = 0
+    @braille_value_array = []
 
   end
 
   def braille_to_letter
     @braille_lines = @braille_data.split("\n")
     @counter = @braille_lines[0].length/2
+    top = @braille_lines[0]
+    mid = @braille_lines[1]
+    bottom = @braille_lines[2]
+    @braille_value_array << top+mid+bottom
+
     binding.pry
 
   end
